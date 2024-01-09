66d21daded7754e24698b1673683b1a8960ca0e2
Tyler Green <tyler-green@live.com>
2024-01-09 17:11:36 -0600

    Improved bomb marble to flash

---
+++ Assets/Scripts/Marble/BombMarble.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using UnityEngine;
+
+public class BombMarble : Marble
+{
+    public Material flashingMaterial;
+    private Material initialMaterial;
+    private Renderer marbleRenderer;
+    private bool isFlashing;
+    private float flashDuration = 0.2f;
+    private Vector3 originalScale;
+    private float scaleMultiplier = 1.2f;
+
+    new void Start()
+    {
+        base.Start();
+        marbleRenderer = GetComponent<Renderer>();
+        initialMaterial = marbleRenderer.material;
+        originalScale = transform.localScale;
+        StartCoroutine(FlashRoutine());
+    }
+
+    IEnumerator FlashRoutine()
+    {
+        while (true)
+        {
+            yield return Flash(flashDuration, isFlashing ? initialMaterial : flashingMaterial);
+            isFlashing = !isFlashing;
+        }
+    }
+
+    IEnumerator Flash(float duration, Material targetMaterial)
+    {
+        float elapsedTime = 0;
+        Material startMaterial = marbleRenderer.material;
+        Vector3 startScale = transform.localScale;
+        Vector3 targetScale = isFlashing ? originalScale : originalScale * scaleMultiplier;
+
+        while (elapsedTime < duration)
+        {
+            elapsedTime += Time.deltaTime;
+            marbleRenderer.material.Lerp(startMaterial, targetMaterial, elapsedTime / duration);
+            transform.localScale = Vector3.Lerp(startScale, targetScale, elapsedTime / duration);
+            yield return null;
+        }
+
+        marbleRenderer.material = targetMaterial;
+        transform.localScale = targetScale;
+    }
+}
